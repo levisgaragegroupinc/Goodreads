91a76cd5c1c86d97a14b7426f59df8a402c76ba8
Travis Bates <tbateswsu@gmail.com>
2022-07-24 15:57:16 -0700

    feat: Added query for me.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,17 @@ const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userProfile = await User.findOne({
+          _id: context.user._id,
+        }).select("-__v -password");
+        return userProfile;
+      }
+      throw new AuthenticationError("You are not logged in!");
+    },
+  },
   Mutation: {},
 };
 
