618d16b85c7a97a0af7c476b7a076e9eaa5f7189
Travis Bates <tbateswsu@gmail.com>
2022-07-24 15:50:03 -0700

    feat: added code for typeDefs.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    bookId: String!
+    title: String!
+    description: String
+    authors: [String]
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input SaveGoodRead {
+    bookId: String
+    title: String
+    description: String
+    authors: [String]
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveGoodRead): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
