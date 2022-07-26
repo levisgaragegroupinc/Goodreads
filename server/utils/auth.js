c209fb4b2c61b7fe653cdcd4abc8f1a7d5c77dd7
Travis Bates <tbateswsu@gmail.com>
2022-07-25 18:53:52 -0700

    debug: resolvers and auth.js.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,7 +23,7 @@ module.exports = {
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      return (req.user = data);
     } catch {
       console.log("Invalid token");
       return req;
