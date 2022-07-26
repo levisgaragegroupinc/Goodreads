654113b902dbfb228ce7d00584558e99017f8a85
Travis Bates <tbateswsu@gmail.com>
2022-07-26 13:18:55 -0700

    debug: refactored SavedBooks code.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,49 +7,18 @@ import {
   Button,
 } from "react-bootstrap";
 import { useMutation, useQuery } from "@apollo/client";
-
-// import { getMe, deleteBook } from '../utils/API';
 import Auth from "../utils/auth";
-import { removeBookId, saveBookIds } from "../utils/localStorage";
+import { removeBookId } from "../utils/localStorage";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME);
-
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-
   let userData = {};
+  const { loading, data } = useQuery(GET_ME);
 
   userData = data?.me || {};
 
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error("something went wrong!");
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -59,14 +28,15 @@ const SavedBooks = () => {
     }
     try {
       const response = await removeBook({
-        variables: { bookId: bookId },
+        variables: { bookId },
       });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
+
+      // if (!response.ok) {
+      //   throw new Error("something went wrong!");
+      // }
     } catch (err) {
       console.error(err);
     }
@@ -77,8 +47,8 @@ const SavedBooks = () => {
     return <h2>LOADING...</h2>;
   }
 
-  const savedBookIds = userData.savedBooks.map((book) => book.bookId);
-  saveBookIds(savedBookIds);
+  // const savedBookIds = userData.savedBooks.map((book) => book.bookId);
+  // saveBookIds(savedBookIds);
 
   return (
     <>
