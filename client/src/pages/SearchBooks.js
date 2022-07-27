b8eb5797b50f605571d3c9a937a2ec950bc18cee
Travis Bates <tbateswsu@gmail.com>
2022-07-26 20:42:48 -0700

    Clean: code review and clean.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -24,13 +24,14 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  // use mutation to save book
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
-
-  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -86,7 +87,7 @@ const SearchBooks = () => {
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
-      console.error("WHAT IS HAPPENING!", err);
+      console.error(err);
     }
   };
 
