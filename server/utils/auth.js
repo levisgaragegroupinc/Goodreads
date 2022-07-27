a63fccbc8e7731f708b1a355747e78c079ba2ffe
Travis Bates <tbateswsu@gmail.com>
2022-07-26 21:27:24 -0700

    debug: modified the return on the authmiddleware that was not returning user data.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,15 +23,12 @@ module.exports = {
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      return (req.user = data);
+      req.user = data;
     } catch {
       console.log("Invalid token");
-      return req;
       // return res.status(400).json({ message: "invalid token!" });
     }
-
-    // send to next endpoint
-    // next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
